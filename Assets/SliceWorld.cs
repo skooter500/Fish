eafc6304805f48f9af8b9fb75f3d6413bf6dbd8b
Bryan Duggan <skooter500@gmail.com>
2015-05-15 18:43:19 +0100

    Bla

--- Assets/SliceWorld.cs
+++ Assets/SliceWorld.cs
@@ -30,8 +30,10 @@ public class SliceWorld : MonoBehaviour {
                 Vector3 position = new Vector3();
                 position.x = transform.position.x + Mathf.Sin(theta) * radius * ring;
                 position.z = transform.position.z + Mathf.Cos(theta) * radius * ring;
-                position.y = position.y + Random.Range(-200, 200);
+                position.y = transform.position.y;
+                position.y += Random.Range(-200, 200);
                 gameObject.transform.position = position;
+                
                 gameObject.transform.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * theta, Vector3.up);
                 sliceForm.size = new Vector3(1000, 3000, 1000);
                 sliceForm.sliceCount = new Vector2(20, 20);
